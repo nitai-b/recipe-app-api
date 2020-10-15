44878d29226824e7d981a4509832ce46556df010
nitai <nitaibarran5@gmail.com>
2020-10-14 22:04:20 -0400

    did a test and wrote code for creating a super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -9,13 +10,12 @@ class ModelTests(TestCase):
         password = 'testpass123'
 
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
     def test_new_user_email_normalized(self):
         """test the email for a new user is normalized"""
@@ -28,3 +28,13 @@ class ModelTests(TestCase):
         """test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_super_user(self):
+        """test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@nitaibarran.com",
+            "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
